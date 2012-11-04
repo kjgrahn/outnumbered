78ac7b27e1f726fe8eb9b6fbb9c091a2195bcf6a
Jörgen Grahn <grahn+src@snipabacken.se>
2012-11-04 08:17:30 +0100

    Gresabladet-specific stuff -- remove. Also include textread from 'sockutil'.

---
+++ textread.cc
@@ -0,0 +1,141 @@
+/* $Id: textread.cc,v 1.6 2010-02-27 08:29:13 grahn Exp $
+ *
+ * Copyright (c) 2010 Jörgen Grahn
+ * All rights reserved.
+ *
+ */
+#include "textread.h"
+
+#include <algorithm>
+#include <cstring>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+
+using namespace sockutil;
+
+
+TextReader::TextReader(int fd, const std::string& endline)
+    : fd_(fd),
+      endline_(endline),
+      p_(buf),
+      q_(p_+sizeof buf),
+      a_(p_),
+      b_(p_),
+      eof_(false),
+      errno_(0)
+{}
+
+
+TextReader::TextReader(const TextReader& other)
+    : fd_(other.fd_),
+      endline_(other.endline_),
+      p_(buf),
+      q_(p_+sizeof buf),
+      a_(p_),
+      b_(p_+(other.b_-other.a_)),
+      eof_(other.eof_),
+      errno_(other.errno_)
+{
+    std::copy(other.a_, other.b_, a_);
+}      
+
+
+TextReader& TextReader::operator= (const TextReader& other)
+{
+    fd_ = other.fd_;
+    endline_ = other.endline_;
+    a_ = p_;
+    b_ = p_ + (other.b_-other.a_);
+    eof_ = other.eof_;
+    errno_ = other.errno_;
+    if(other.a_!=a_) std::copy(other.a_, other.b_, a_);
+    return *this;
+}
+
+
+void TextReader::feed()
+{
+    if(a_!=b_ && a_!=p_) {
+	std::copy(a_, b_, p_);
+    }
+    b_ -= (a_-p_);
+    a_ = p_;
+
+    const ssize_t n = recv(fd_, b_, q_-b_, 0);
+    if(n==-1) {
+	switch(errno) {
+	case EAGAIN:
+	case EINTR:
+	    break;
+	default:
+	    eof_ = true;
+	    errno_ = errno;
+	}
+    }
+    else if(n==0) {
+	eof_ = true;
+    }
+    else {
+	b_+=n;
+    }
+}
+
+
+size_t TextReader::read(char*& begin, char*& end)
+{
+    char* const c = std::search(a_, b_,
+				endline_.begin(), endline_.end());
+    if(c==b_) {
+	if(eof_) {
+	    /* The last of the wine ... return it even without
+	     * an endline marker.
+	     *
+	     * XXX Slight loophole here -- eof_ may be a false one
+	     * caused by an overlong line. But we shouldn't have
+	     * kept reading in that case.
+	     */
+	    begin = a_;
+	    end = b_;
+	    a_ = b_;
+	    return (end-begin);
+	}
+
+	if(a_==p_ && b_==q_) {
+	    /* The buffer is full and yet there is no endline, i.e.  we've
+	     * found a line that's too long to read.  We could read it as
+	     * a fragment, but for now choose the other option: treat it
+	     * as an error.
+	     */
+	    eof_ = true;
+	}
+
+	return 0;
+    }
+
+    begin = a_;
+    end = c + endline_.size();
+    a_ = end;
+
+    return (end-begin);
+}
+
+
+std::string TextReader::read()
+{
+    char* a = 0;
+    char* b = 0;
+    read(a, b);
+    return std::string(a, b);
+}
+
+
+/**
+ * Same as strerror(error()).
+ */
+const char* TextReader::strerror() const
+{
+    return std::strerror(error());
+}
---
+++ textread.h
@@ -0,0 +1,76 @@
+/* -*- c++ -*-
+ * $Id: textread.h,v 1.5 2010-02-27 08:03:59 grahn Exp $
+ *
+ * Copyright (c) 2010 Jörgen Grahn
+ * All rights reserved.
+ *
+ */
+#ifndef SOCKET_TEXTREAD_H_
+#define SOCKET_TEXTREAD_H_
+
+#include <cstdlib>
+#include <string>
+
+namespace sockutil {
+
+    /**
+     * Reading lines of text from a TCP socket, which may be in
+     * blocking or non-blocking mode. Or more generally, reading data
+     * with some kind of single- or multi-octet end-of-record marker;
+     * "text" is just a special case with '\n' or "\r\n" as that
+     * marker.  Useful with protocols like SMTP and HTTP.  Also
+     * tolerates '\0' in the stream.
+     *
+     * Usage:
+     *
+     * feed() calls recv(2) exactly once (so that it can be used with
+     * select(2) and blocking sockets). It may read some stream data
+     * or set eof().
+     *
+     * After feed(), one of the read() functions should be called
+     * repeatedly until it returns 0 or the empty string,
+     * respectively. Each string returned includes the endline, except
+     * possibly the last one on the stream.  Strings longer than the
+     * fixed-size internal buffer aren't supported: if one comes it
+     * will look like EOF. (Most protocols specify a minimum line length
+     * which you have to support.)
+     *
+     * Whenever eof() is set, there is no point in calling feed()
+     * again, but there may be lines left to read() from the buffer.
+     *
+     * When both eof() and error() are set, error() is the errno which
+     * caused feed() to fail, and strerror() is the error text.
+     * 
+     */
+    class TextReader {
+    public:
+	TextReader(int fd, const std::string& endline);
+	TextReader(const TextReader&);
+	TextReader& operator= (const TextReader&);
+
+	void feed();
+
+	size_t read(char*& begin, char*& end);
+	std::string read();
+
+	bool eof() const { return eof_; }
+	int error() const { return errno_; }
+	const char* strerror() const;
+
+    private:
+	TextReader();
+
+	int fd_;
+	std::string endline_;
+	char buf[8000];
+	char* const p_;
+	char* const q_;
+	char* a_;
+	char* b_;
+	bool eof_;
+	int errno_;
+    };
+
+}
+
+#endif
