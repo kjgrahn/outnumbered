03e7346b5a2e967f214158cd7ec36c82cb8b82c3
Jörgen Grahn <grahn+src@snipabacken.se>
2012-12-31 10:58:21 +0100

    make the filter stuff build and link

--- filter.cc
+++ filter.cc
@@ -72,7 +72,7 @@ size_t Backlog::write(int fd)
     const iovec ovec = iov(tmp);
 
     ssize_t n = writev(fd, &ovec, 1);
-    if(n==-1 && errno==EAGAIN) {
+    if(n==-1 && errno!=EAGAIN) {
 	throw WriteError(errno);
     }
 
@@ -92,7 +92,7 @@ size_t Backlog::write(int fd, const Blob& a)
 			    iov(a) };
 
     ssize_t n = writev(fd, ovec, 2);
-    if(n==-1 && errno==EAGAIN) {
+    if(n==-1 && errno!=EAGAIN) {
 	throw WriteError(errno);
     }
 
@@ -113,11 +113,29 @@ size_t Backlog::write(int fd, const Blob& a, const Blob& b)
 			    iov(b) };
 
     ssize_t n = writev(fd, ovec, 3);
-    if(n==-1 && errno==EAGAIN) {
+    if(n==-1 && errno!=EAGAIN) {
 	throw WriteError(errno);
     }
 
     return salvage(v, ovec, 3, n);
+}
+
+
+size_t Backlog::write(int fd, const Blob& a, const Blob& b, const Blob& c)
+{
+    Buf tmp;
+    tmp.swap(v);
+    const iovec ovec[4] = { iov(tmp),
+			    iov(a),
+			    iov(b),
+			    iov(c) };
+
+    ssize_t n = writev(fd, ovec, 4);
+    if(n==-1 && errno!=EAGAIN) {
+	throw WriteError(errno);
+    }
+
+    return salvage(v, ovec, 4, n);
 }
 
 
