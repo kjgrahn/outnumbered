deb4b72c7387dc21f34959d8cad4814168f4819f
Jörgen Grahn <grahn+src@snipabacken.se>
2013-03-18 23:46:10 +0100

    Response: bug fix

--- response.cc
+++ response.cc
@@ -1,13 +1,31 @@
-/* Copyright (c) 2012 Jörgen Grahn
+/* Copyright (c) 2012, 2013 Jörgen Grahn
  * All rights reserved.
  *
  */
 #include "response.h"
 #include <cassert>
+#include <cctype>
+
+namespace {
+
+    std::string rstrip(const std::string& s)
+    {
+	std::string r = s;
+
+	const char* const p = s.c_str();
+	const char* q = p + s.size();
+	while(p!=q) {
+	    if(!isspace(*(q-1))) break;
+	    q--;
+	}
+	r.resize(q-p);
+	return r;
+    }
+}
 
 
 Response::Response(const std::string& request)
-    : src(request),
+    : src(rstrip(request)),
       done_(false)
 {}
 
